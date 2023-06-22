7f3e9347a07f1342e2de3adfad9b9554003bca3e
Vysakh P <vysakhp17@gmail.com>
2023-06-22 20:33:41 +0530

    get request handling

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
+const courses = [
+    { id: 1, name: 'MCA' },
+    { id: 2, name: 'BCA' },
+    { id: 3, name: 'BBA' }
+];
+
+app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.get('/api/courses', (req,res) =>{
+app.get('/api/courses', (req, res) => {
     res.send('MCA');
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(v => v.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('couldnot find any courses');
+    res.send(course);
 });
 
 app.get('/api/post/:id/:name', (req, res) => {
@@ -25,4 +33,4 @@ app.get('/api/post/:id/', (req, res) => {
 
 //PORT
 const port = process.env.port || 3000;
-app.listen(port, () => console.log(`Listening to the port ${port}...`) )
+app.listen(port, () => console.log(`Listening to the port ${port}...`))
